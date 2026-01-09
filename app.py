4a1e4f81be69e2d14e439ad32080de29927a600c
Arpit-ai-ops <arpit846556@gmail.com>
2026-01-10 02:19:40 +0530

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+nltk.download('punkt')
+nltk.download('stopwords')
+
 ps = PorterStemmer()
 
 
@@ -51,4 +54,5 @@ if st.button('Predict'):
     if result == 1:
         st.header("Spam")
     else:
+
         st.header("Not Spam")
